92573a4693195287bf03f4dbb3bed51873a4d2a5
titian1012 <titian1@yahoo.com>
2015-09-01 17:23:32 -0500

    ConstantsAndVariables
    Christian Ramos

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var Variable1 = 10
+var Variable2 = 20.50
+var Variable3 = 20.5052752
+
+let Constant1 = 50
+let Constant2 = 3.50
+let Constant3 = 26.507282782
+
+let ExplicitConstant1: String = "Hello"
+let ExplicitConstant2: Float = 5.20
+let ExplicitConstant3: Double = 2.6578914
+
+let ChangingConstant = 12
+//ChangingConstant = 2 does not let me change it. Constants can not be changed.
+
+let ExplicitChangingConstant: Float = 4
+
+/*Constantss do not change. When I explicitly made the second constant a float
+it shows up as just 4 on the side and when you click on the  show reults it pops
+up a box that shows it as 4.0.*/
